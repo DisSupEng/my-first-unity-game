fcd69aba47830e5ba4f970b88b0cb11bc3065da4
Connor <connora.wellington18@gmail.com>
2019-03-08 16:03:45 +1300

    Added code for detecting when the player hits an obstacle

---
+++ Basic 3d game attempt/Assets/PlayerCollisions.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class PlayerCollisions : MonoBehaviour
+{
+    public void OnCollisionEnter(Collision info)
+    {
+        if(info.collider.tag == "Obstacle")
+        {
+            Debug.Log("We hit an obstacle");
+        }
+    }
+}
