0d28ed83eed51434932c12c91d0a919656ed758a
Connor <connora.wellington18@gmail.com>
2019-03-08 15:20:49 +1300

    Added initial follow player script that prints the players position.

--- Basic 3d game attempt/Assets/PlayerMovement.cs
+++ Basic 3d game attempt/Assets/PlayerMovement.cs
@@ -19,12 +19,8 @@ public class PlayerMovement : MonoBehaviour
     {
         rb.AddForce(0, 0, forwardForce * Time.deltaTime);
         if(Input.GetKey("a")) {
-            Debug.Log("a key pressed");
-            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0);
         }
         if(Input.GetKey("d")) {
-            Debug.Log("d key pressed");
-            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0);
         }
     }
 }
