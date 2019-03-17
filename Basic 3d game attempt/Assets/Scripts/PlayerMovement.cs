6f3e550ac7257bda4c94511ce620fc7885f04135
Connor <connora.wellington18@gmail.com>
2019-03-17 21:10:09 +1300

    Fixed issue with sideways movement being to fast

--- Basic 3d game attempt/Assets/Scripts/PlayerMovement.cs
+++ Basic 3d game attempt/Assets/Scripts/PlayerMovement.cs
@@ -6,19 +6,19 @@ public class PlayerMovement : MonoBehaviour
 {
 
     public Rigidbody rb;
-    public float forwardForce = 200f;
-    public float sidewaysForce = 100f;
+    public float forwardForce;
+    public float sidewaysForce;
 
     // Update is called once per frame
     void FixedUpdate()
     {
-        rb.AddForce(0, 0, forwardForce * Time.deltaTime);
+        rb.AddForce(0, 0, forwardForce * Time.deltaTime, ForceMode.Acceleration);
 
         if(Input.GetKey(KeyCode.LeftArrow)) {
-            rb.AddForce(-sidewaysForce, 0, 0, ForceMode.VelocityChange);
+            rb.AddForce(-sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         if(Input.GetKey(KeyCode.RightArrow)) {
-            rb.AddForce(sidewaysForce, 0, 0, ForceMode.VelocityChange);
+            rb.AddForce(sidewaysForce * Time.deltaTime, 0, 0, ForceMode.VelocityChange);
         }
         // transform refers to the player's position. We can reference it like this because this script is linked to the player
         if (transform.position.y < -1)
